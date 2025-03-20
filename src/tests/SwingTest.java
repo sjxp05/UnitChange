c491aab4e62520aea710c622cb0877f7c8e32e24
j2iiin <jxxniie05@gmail.com>
2025-03-20 14:17:25 +0900

    헤헤 재밌다~ 	modified:   src/tests/SwingTest.java

--- src/tests/SwingTest.java
+++ src/tests/SwingTest.java
@@ -3,8 +3,24 @@ package tests;
 import java.awt.*;
 import javax.swing.*;
 
-public class SwingTest {
+public class SwingTest extends JFrame {
+    public SwingTest() {
+        setTitle("New frame");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container pane = getContentPane();
+        pane.setLayout(null);
+
+        JButton bt1 = new JButton("click me");
+        bt1.setLocation(30, 20);
+        bt1.setSize(100,50);
+        pane.add(bt1);
+
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        
+        new SwingTest();
     }
 }
