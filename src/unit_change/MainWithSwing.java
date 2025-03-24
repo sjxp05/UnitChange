8272a730dbe1aac305f83ea14780a55a41bfb364
j2iiin <jxxniie05@gmail.com>
2025-03-24 13:55:06 +0900

    new file:   src/unit_change/MainWithSwing.java

---
+++ src/unit_change/MainWithSwing.java
@@ -0,0 +1,13 @@
+package unit_change;
+
+import unit_change.units.*;
+
+public class MainWithSwing {
+    public MainWithSwing() {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
